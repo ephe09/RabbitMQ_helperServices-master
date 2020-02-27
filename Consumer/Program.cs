fc189dc38480f7ea399c7ef7755711308208ae9f
Mustafa GÜLMEZ <mustafa.gulmez@vbt.com.tr>
2020-02-27 16:25:15 +0300

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using Models;
+using Newtonsoft.Json;
+using RabbitMQ.Client.Events;
+using RabbitMQService;
+using RabbitMQService.Abstract;
+
+namespace _ConsumerApp
+{
+    class Program
+    {
+
+        public delegate void delmethod(object sender, BasicDeliverEventArgs e);
+
+        static void Main(string[] args)
+        {
+            IRabbitConsumer consumer = new RabbitConsumer();
+
+            // Using first sample
+            consumer.Subscribe(Queens.QueenTopic, Consumer_Received_Sample_Body);
+
+            // Using second sample
+            //consumer.Subscribe(Queens.QueenTopic, Consumer_Received_Sample_EventHandler);
+
+            Console.ReadKey();
+        }
+
+        public static void Consumer_Received_Sample_Body(byte[] body)
+        {
+            var message = Encoding.UTF8.GetString(body);
+            var deseriliazeObject = JsonConvert.DeserializeObject<SampleDataModel>(message);
+            Console.WriteLine(JsonConvert.SerializeObject(deseriliazeObject));
+        }
+
+        public static void Consumer_Received_Sample_EventHandler(object sender, BasicDeliverEventArgs args)
+        {
+            var message = Encoding.UTF8.GetString(args.Body);
+            var deseriliazeObject = JsonConvert.DeserializeObject<SampleDataModel>(message);
+            Console.WriteLine(JsonConvert.SerializeObject(deseriliazeObject));
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Models;
+using RabbitMQService;
+using RabbitMQService.Abstract;
+
+namespace _PublisherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRabbitPublisher _publisher = new RabbitPublisher();
+
+            _publisher.PublishQueen(new SampleDataModel
+            {
+                Id = 1,
+                Age = 32,
+                Name = "Mustafa",
+                Surname = "GÜLMEZ",
+                QueenGuid = Guid.NewGuid()
+            });
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
